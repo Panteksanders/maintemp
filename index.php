bb8cb0d8faab1919936501aec7e344879dac4d11
Paulsanders007 <110789816+Paulsanders007@users.noreply.github.com>
2022-09-27 09:51:43 -0500

    Update index.php

--- index.php
+++ index.php
@@ -26,8 +26,8 @@ function doDecrypt($mail)
         <meta charset="utf-8"> <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <link rel="stylesheet" type="text/css" href="assets/style/style.css">
-    <script src="assets/js/script.js"></script>   
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <script src="script.js"></script>   
 
        
 </head>
